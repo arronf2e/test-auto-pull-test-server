f1343ffc72f447716ac3f55483abd78a02c369db
null <191446367@qq.com>
2018-05-24 17:25:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,60 @@
-const fs = require('fs');
 const path = require('path');
-
-const Koa = require('koa');
-const Router = require('koa-router');
-
-const app = new Koa();
-const router = new Router();
-
-const nodemailer = require('nodemailer');
-const rp = require('request-promise');
-
-// 使用child_process来执行shell命令
 const exec = require('child_process').exec;
+const http = require('http');
+const readline = require('readline');
+const axios = require('axios');
 
-const _exec = (diskName, folderName, sysName) => {
-	return new Promise((resolve, reject) => {
-		exec('git checkout test', {cwd: path.resolve(diskName + ':/' + folderName + '/' + sysName)}, (err, stdout, stderr) => {
-			exec('git pull origin test', {cwd: path.resolve(diskName + ':/' + folderName + '/' + sysName)}, (error, stdout, stderr) => {
-				if(error) {
-					reject(error);
-				}
-				resolve(stdout);
-			})
-		})
-	})
-}
+const baseDeployUrl = 'http://192.168.3.112:9999/autopull/d/web/';
 
-// 邮件发送配置
-const config = {
-	host: "smtp.163.com", // 主机
-	secureConnection: true, // 使用 SSL
-	port: 465, // SMTP 端口 
-	auth: {
-		user: 'arronf2e@163.com',
-		pass: 'zhanghao2202' // 注意这个不是邮箱的登录密码，是第三方授受密码！！！！
-	}
-}
-
-const _sendMail = (sysName, html) => {
-	let options = {
-		from: 'arronf2e@163.com', //  这里一定要和上面的邮箱保持一致！！！
-		to: '191446367@qq.com',
-		subject: sysName + ' test pull successful !',
-		html: html
-	}
-	const transporter = nodemailer.createTransport(config);
-	return new Promise((resolve, reject) => {
-		transporter.sendMail(options, (err, info) => {
-			if(err) {
-				reject(err);
-			}
-			resolve(info)
-		})
-	})
-}
-
-const _sendTodd = (sysName, html) => {
-	return new Promise((resolve, reject) => {
-		let options = {
-			method: 'POST',
-			uri: 'https://oapi.dingtalk.com/robot/send?access_token=4b16bad73a375131b842c7124134463c785fb37a96098c35e4998a9d8f4b6bea',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: {
-				'msgtype': 'text',
-				'text': {
-					'content': sysName + ' test pull successful!' + '\n' + html
-				}
-			},
-			json: true // Automatically stringifies the body to JSON
-		};
-		rp(options)
-			.then(body => {
-				resolve(body)
-			})
-			.catch(err => {
-				reject(err)
-			});
-	})
-}
-
-router.get('/', (ctx, next) => {
-	ctx.body = 'hello world';
-})
-
-router.get('/autopull/:disk/:folder/:sys', async(ctx, next) => {
-	let sysName = ctx.params.sys,
-		folderName = ctx.params.folder,
-		diskName = ctx.params.disk;
-	let data = await _exec(diskName, folderName, sysName);
-	let sendToddResult = await _sendTodd(sysName, data);
-	ctx.body = sendToddResult;
-	// let sendResult = await _sendMail(sysName, data);
-	// ctx.body = sendResult;
-})
-
-app.use(router.routes())
-   .use(router.allowedMethods());
-
-app.listen(9999, () => {
-	console.log('Server is running at port 9999!')
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
 });
+
+rl.question('请输入要发布到test的分支: ', (branch) => {
+	// TODO: Log the answer in a database
+	_exec(branch);
+	// rl.close();
+});
+
+const _exec = async (branch) => {
+	// let branch = JSON.parse(process.env.npm_config_argv).cooked[2];
+	let project = __dirname.split('\\')[__dirname.split('\\').length - 1];
+	try {
+		await deployStep('git checkout test', '切换test分支');
+		await deployStep('git pull origin test', '更新test分支');
+		await deployStep(`git merge ${branch}`, `合并${branch}分支到test分支`);
+		await deployStep('npm run build:test', '打包');
+		await deployStep('git add .', 'git add');
+		await deployStep(`git commit -m "test"`, 'git commit');
+		await deployStep('git push origin test', '发布');
+		await deployStep(`git checkout ${branch}`, `切换${branch}分支`);
+		updateRemote(project);
+	}catch(err) {
+		console.log(`\n${err}`);
+		process.exit();
+	}
+}
+
+const deployStep = (step, stateMsg) => {
+	return new Promise((resolve, reject) => {	
+		exec(step, {cwd: path.resolve(__dirname), maxBuffer: 5000 * 1024}, (error, stdout, stderr) => {
+			if(error) {
+				reject(`\n${stateMsg}：失败，Error: ${error}`);
+			}
+			console.log(`\n${stateMsg}成功`);
+			resolve();
+		})
+	})
+}
+
+const updateRemote = async (project) => {
+	try {
+		let result = await axios.get(`${baseDeployUrl}${project}`);
+	}catch(err) {
+		console.log(`\n远程代码更新失败：${err}`);
+		process.exit();
+	}
+	console.log(`\n远程代码更新成功`);
+	process.exit();
+}
